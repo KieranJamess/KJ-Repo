6ab15cb176a45e0bcb6bb742130d794e0ff3d467
Kieran James <onlyeverything@outlook.com>
2020-05-21 22:48:43 +0100

    adds comments to ensure the script works

--- Python/odds-on-generator.py
+++ Python/odds-on-generator.py
@@ -1,7 +1,8 @@
+# This was my first little test gimmick after learning a little about Python to test my own knowledge. 
+# To get this to work, you first need to import the randint from random using <from random import randint>.
 Result = input('What number would you like? ')
 Start = input("What start number would you like? ")
 End = input("What end number would you like? ")
-
 Range = (randint(int(Start),int(End)))
 
 if Range == int(Result):
