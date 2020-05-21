ebdeac33860c6ccf48065df8b56982a490e30681
Kieran James <onlyeverything@outlook.com>
2020-05-21 23:41:10 +0100

    adds randint into config

--- Python/odds-on-generator.py
+++ Python/odds-on-generator.py
@@ -1,5 +1,8 @@
 # This was my first little test gimmick after learning a little about Python to test my own knowledge. 
-# To get this to work, you first need to import the randint from random using <from random import randint>.
+
+# Imports randint for random number generation.
+from random import randint
+
 Result = input('What number would you like? ')
 Start = input("What start number would you like? ")
 End = input("What end number would you like? ")
