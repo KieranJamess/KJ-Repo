0a68acae8019f7084849f99de9bf5a535695e591
Kieran James <onlyeverything@outlook.com>
2020-05-22 01:24:31 +0100

    adds math game python script to github

---
+++ Python/math-game.py
@@ -0,0 +1,45 @@
+from random import randint
+import random
+number1 = randint(1,12)
+number2 = randint(1,12)
+optionList = ['*','+','/','-']
+option = random.choice(optionList)
+
+
+print (f'What is {number1} {option} {number2}?\nPlease answer to the nearest whole number. ')
+
+result = input('Answer = ')
+
+if option == '+':
+    answer = number1 + number2
+    if answer == int(result):
+        print (f'Well Done, the answer was {answer}')
+    
+    else:
+        print (f'You suck, the answer was {answer}')
+
+if option == '/':
+    answer = number1 / number2
+    answer = int(round(answer))
+    if answer == int(result):
+        print (f'Well Done, the answer was {answer}')
+    
+    else:
+        print (f'You suck, the answer was {answer}')
+        
+if option == '*':
+    answer = number1 * number2
+    if answer == int(result):
+        print (f'Well Done, the answer was {answer}')
+    
+    else:
+        print (f'You suck, the answer was {answer}')
+        
+if option == '-':
+    answer = number1 - number2
+    if answer == int(result):
+        print (f'Well Done, the answer was {answer}')
+    
+    else:
+        print (f'You suck, the answer was {answer}')
+
