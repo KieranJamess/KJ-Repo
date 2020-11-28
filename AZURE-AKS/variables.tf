f34d8b54ccc593d49e2becf8ea8bfe4a7d73ef4e
KieranJamess <onlyeverything@outlook.com>
2020-11-28 18:07:32 +0000

    adds basic template for aks cluster

---
+++ AZURE-AKS/variables.tf
@@ -0,0 +1,23 @@
+variable "resourcename" {
+  default = "test"
+}
+
+variable "clustername" {
+  default = "akscluster-kj"
+}
+
+variable "location" {
+  default = "northeurope"
+}
+
+variable "dnspreffix" {
+  default = "aks"
+}
+
+variable "size" {
+  default = "Standard_D2_v2"
+}
+
+variable "agentnode" {
+  default = "1"
+}
